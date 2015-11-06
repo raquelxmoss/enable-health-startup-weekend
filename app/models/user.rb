52c92f7a3d1e0b9d65b5d180949e5ce7f6324a78
Raquel Moss <raquelxmoss@gmail.com>
2015-11-07 10:53:21 +1300

    add city to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,8 @@ class User < ActiveRecord::Base
   has_many :skills, through: :user_skills
   has_many :sent_messages, class_name: 'Message', foreign_key: 'sender_id'
   has_many :received_messages, class_name: 'Message', foreign_key: 'recipient_id'
+
+  def self.cities
+  	["Auckland", "Wellington", "Christchurch", "Dunedin"]
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151106195540) do
+ActiveRecord::Schema.define(version: 20151106214816) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20151106195540) do
     t.string   "phone"
     t.text     "address"
     t.text     "name"
+    t.string   "city"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
