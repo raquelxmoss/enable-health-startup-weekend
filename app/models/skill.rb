030753c688e33bc87fcab477876b882a403317af
Raquel Moss <raquelxmoss@gmail.com>
2015-11-07 10:21:19 +1300

    comment normalize title

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -2,9 +2,9 @@ class Skill < ActiveRecord::Base
   has_many :user_skills
   has_many :users, through: :user_skills
 
-  before_action :normalize_title
+  # before_action :normalize_title
 
-  def normalize_title
-  	title.titelize
-  end
+  # def normalize_title
+  # 	title.titelize
+  # end
 end
