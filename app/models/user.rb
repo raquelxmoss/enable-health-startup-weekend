318ba911dc92335fca9a8b62969971671a3040a3
Raquel Moss <raquelxmoss@gmail.com>
2015-11-07 11:04:44 +1300

    Add validation for city

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@ class User < ActiveRecord::Base
   has_many :sent_messages, class_name: 'Message', foreign_key: 'sender_id'
   has_many :received_messages, class_name: 'Message', foreign_key: 'recipient_id'
 
+  validates :city, presence: true
+
   def self.cities
   	["Auckland", "Wellington", "Christchurch", "Dunedin"]
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+	it 'should have a city' do
+		user = User.create(name: Faker::Name.name, email: Faker::Internet.email, 
+											 password: 'password', password_confirmation: 'password', 
+											 address: Faker::Address.street_address, phone: Faker::PhoneNumber.cell_phone)
+		expect(user.errors.full_messages).to include("City can't be blank")
+	end
 end
