f795a5fa6f8b915567435a569cefe3507ff6339f
Raquel Moss <raquelxmoss@gmail.com>
2015-11-07 08:43:50 +1300

    Oops, we need postgres

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+10.times do
+	User.create(name: Faker::Name.name, password: 'password', 
+							password_confirmation: 'password', address: Faker::Address.street_address,
+							phone: Faker::PhoneNumber.cell_phone)
+end
+
+User.all.each do |u|
+	u.skills.create(skill: Faker::Company.profession)
+end
