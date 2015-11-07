f858227f2427b0c91d2a04ec924ee70dcb274551
adam <adamron1@hotmail.com>
2015-11-07 17:32:37 +1300

    added contact and about

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   root 'application#index'
+  get '/about', to: 'application#about'
+  get '/contact',to: 'application#contact'
   devise_for :users
 
   resources :listings
