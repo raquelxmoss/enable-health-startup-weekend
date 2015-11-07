8a882abbbf3d9bbafb5dd66aae376af5c637dea6
Raquel Moss <raquelxmoss@gmail.com>
2015-11-07 13:12:12 +1300

    titelize works

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,9 +1,9 @@
 class Listing < ActiveRecord::Base
   belongs_to :user
 
-  # before_action :normalize_title
+  before_create :normalize_title
 
-  # def normalize_title
-  # 	title.titelize
-  # end
+  def normalize_title
+  	self.title = self.title.titleize
+  end
 end
