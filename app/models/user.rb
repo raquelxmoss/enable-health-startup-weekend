2e1a04e3c1bda3a4fbab68eb8430458d5e04b27e
Raquel Moss <raquelxmoss@gmail.com>
2015-11-07 18:10:35 +1300

    more niceties

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,9 @@ class User < ActiveRecord::Base
   has_many :listings
   has_many :sent_messages, class_name: 'Message', foreign_key: 'sender_id'
   has_many :received_messages, class_name: 'Message', foreign_key: 'recipient_id'
-  has_attached_file :avatar
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+
 
   def self.cities
   	["Auckland", "Wellington", "Christchurch", "Dunedin"]
