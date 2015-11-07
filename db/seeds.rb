ad773c9095df5a4a01641fb0be6a01fd5977fc54
Raquel Moss <raquelxmoss@gmail.com>
2015-11-07 13:04:22 +1300

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,15 @@
+User.destroy_all
+Listing.destroy_all
+Message.destroy_all
+
 10.times do
 	User.create!(name: Faker::Name.name, password: 'password', 
 							password_confirmation: 'password', address: Faker::Address.street_address,
-							phone: Faker::PhoneNumber.cell_phone, email: Faker::Internet.email)
+							phone: Faker::PhoneNumber.cell_phone, email: Faker::Internet.email, city: User.cities.sample)
 end
 
 User.all.each do |u|
-	u.skills.create(title: Faker::Lorem.word)
+	u.listings.create(title: Faker::Lorem.word, description: Faker::Lorem.paragraph, expiration_date: DateTime.now + 5.days, location: User.cities.sample)
 	u.sent_messages.create(sender_id: u.id, recipient_id: rand(0..User.all.count), body: Faker::Lorem.paragraph, status: 'sent')
 end
 
