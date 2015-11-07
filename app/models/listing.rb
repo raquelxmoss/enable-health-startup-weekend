23fcc47ee146a252876fce34335284072df6d651
Raquel Moss <raquelxmoss@gmail.com>
2015-11-07 13:38:37 +1300

    Add scopes for Listing

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,6 +2,10 @@ class Listing < ActiveRecord::Base
   belongs_to :user
 
   before_create :normalize_title
+	scope :open, -> { where(status: 'open') }
+	scope :active, -> { where("expiration_date > ?", DateTime.now) }
+
+private
 
   def normalize_title
   	self.title = self.title.titleize
